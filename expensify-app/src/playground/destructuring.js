e8f96aced44ee97e1da0bd5b2a9e1935a56cac82
Anjali <anjali.payal69@gmail.com>
2020-09-21 14:23:37 +0200

    ES6 Array Destructuring

--- expensify-app/src/playground/destructuring.js
+++ expensify-app/src/playground/destructuring.js
@@ -19,17 +19,17 @@
 //   console.log(`It's ${temperature} degrees celcius in ${city}.`);
 // }
 
-const book = {
-  title: 'Ego is the Enemy',
-  author: 'Ryan Holiday',
-  publisher: {
-    // name: 'Penguin'
-  }
-};
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     // name: 'Penguin'
+//   }
+// };
 
-const { name: publisherName = 'Self-Published' } = book.publisher;
+// const { name: publisherName = 'Self-Published' } = book.publisher;
 
-console.log(publisherName); // Penguin, Self-Published
+// console.log(publisherName); // Penguin, Self-Published
 
 // !---------------------------!
 // Array destructuring
@@ -39,7 +39,7 @@ console.log(publisherName); // Penguin, Self-Published
 // const [, city, state = 'New York'] = address;
 // console.log(`You are in ${city} ${state}.`);
 
-const item = ['Coffee (iced)', '$3.00', '$3.50', '$3.75'];
+const item = ['Coffee (iced)', '$3.00', '$2.50', '$3.75'];
 const [itemName, , mediumPrice] = item;
 
 console.log(`A medium ${itemName} costs ${mediumPrice}`);
