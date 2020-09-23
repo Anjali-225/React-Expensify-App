2c10764bc067631c612620d194528f98a85a9f21
Anjali <anjali.payal69@gmail.com>
2020-09-23 12:18:02 +0200

    Sorting Redux Data

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -156,6 +156,12 @@ const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
         // convert both strings to lower case
 
         return startDateMatch && endDateMatch && textMatch;
+    }).sort((a, b) => {
+        if (sortBy === 'date') {
+            return a.createdAt < b.createdAt ? 1: -1;
+        } else if (sortBy === 'amount') {
+            return a.amount < b.amount ? 1 : -1;
+        }
     });
 };
 
@@ -175,16 +181,16 @@ store.subscribe(() => {
     console.log(visibleExpenses);
 });
 
-const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: 1000 }));
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: -21000 }));
 const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300, createdAt: -1000 }));
 
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
 // store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
 
-store.dispatch(setTextFilter('rent'));
+// store.dispatch(setTextFilter('rent'));
 
-// store.dispatch(sortByAmount());
+store.dispatch(sortByAmount());
 
 // store.dispatch(sortByDate());
 
